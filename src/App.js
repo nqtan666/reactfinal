660b1adb52e60a1081889828a2bc305866cec0dc
TanNQ <nqtan0610@gmail.com>
2023-09-12 22:54:15 +0700

    chapter 5

--- src/App.js
+++ src/App.js
@@ -1,34 +1,6 @@
-import logo from "./logo.svg";
 import "./App.scss";
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import MyComponent from "./components/MyComponent";
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app-container">
-        Hello World With TanNQ
-        <MyComponent />
-      </div>
-    );
-  }
-}
-// const App = () => {
-//   const count = useSelector((state) => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     // <div className="App">
-//     //   <header className="App-header">
-//     //     <img src={logo} className="App-logo" alt="logo" />
-//     //     <p>Hello World Hoi Dan IT</p>
-//     //     <div>Count = {count}</div>
-//     //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//     //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//     //   </header>
-//     // </div>
-//   );
-// };
+const App = () => {
+  return <div className="app-container">Hello World</div>;
+};
 
 export default App;
