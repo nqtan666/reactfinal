660fc8a2951d240067ee1b071018fb9485d58a54
TanNQ <nqtan0610@gmail.com>
2023-09-22 17:29:25 +0700

    create user connect backend

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,16 +1,23 @@
-import Example from "./ModelCreateUser";
+import { useState } from "react";
+import ModelCreateUser from "./ModelCreateUser";
+import "./ManageUser.scss";
+import { FcPlus } from "react-icons/fc";
+
 const ManageUser = () => {
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
   return (
-    <div classNameName="manage-user-container">
-      <div classNameName="title">ManageUser</div>
-      <div classNameName="users-content">
-        <div>
-          <button>Add user</button>
+    <div className="manage-user-container">
+      <div className="title">ManageUser</div>
+      <div className="users-content">
+        <div className="btn-add-new">
+          <button className="btn btn-primary" onClick={() => handleShow()}>
+            <FcPlus />
+            Add user
+          </button>
         </div>
-        <div>
-          table users
-          <Example />
-        </div>
+        <div className="table-users-container">table users</div>
+        <ModelCreateUser show={show} setShow = {setShow}/>
       </div>
     </div>
   );
--- src/components/Admin/Content/ModelCreateUser.js
+++ src/components/Admin/Content/ModelCreateUser.js
@@ -1,20 +1,72 @@
+import axios from "axios";
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import { FaArrowCircleUp } from "react-icons/fa";
 
-function Example() {
-  const [show, setShow] = useState(false);
+function ModelCreateUser(props) {
+  const { show, setShow } = props;
+  const handleClose = () => {
+    setShow(false);
+    setEmail("");
+    setPassword("");
+    setUserName("");
+    setRole("");
+    setImage("");
+    setPreviewImage("");
+  };
+  // const handleShow = () => setShow(true);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUserName] = useState("");
+  const [role, setRole] = useState("USER");
+  const [image, setImage] = useState("");
+  const [previewImage, setPreviewImage] = useState("");
 
+  const handelUploadImage = (e) => {
+    if (e.target && e.target.files && e.target.files["0"]) {
+      setImage(e.target.files["0"]);
+      setPreviewImage(URL.createObjectURL(e.target.files["0"]));
+    }
+  };
+  const handlSubmitCreateUser = async () => {
+    // validate
+
+    // let data = {
+    //   email : email,
+    //   password :password,
+    //   username :username,
+    //   role :role,
+    //   userImage:image,
+    // }
+    const data = new FormData();
+    data.append("email", email);
+    data.append("password", password);
+    data.append("username", username);
+    data.append("role", role);
+    data.append("userImage", image);
+    let res = await axios.post(
+      "http://localhost:8081/api/v1/participant",
+      data
+    );
+    if (res.status === 200) {
+      handleClose();
+    }
+  };
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
+      {/* <Button variant="primary" onClick={handleShow}>
         Add new user
-      </Button>
+      </Button> */}
 
-      <Modal show={show} onHide={handleClose} size="xl" backdrop="static">
+      <Modal
+        show={show}
+        onHide={handleClose}
+        size="xl"
+        backdrop="static"
+        className="modal-add-user"
+      >
         <Modal.Header closeButton>
           <Modal.Title>Add new user</Modal.Title>
         </Modal.Header>
@@ -26,6 +78,8 @@ function Example() {
                 type="email"
                 className="form-control"
                 placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className=" col-md-6">
@@ -34,23 +88,47 @@ function Example() {
                 type="password"
                 className="form-control"
                 placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
             <div className="col-md-6">
               <label>UserName</label>
-              <input type="text" className="form-control" />
+              <input
+                type="text"
+                className="form-control"
+                value={username}
+                onChange={(e) => setUserName(e.target.value)}
+              />
             </div>
             <div className=" col-md-4">
               <label>Role</label>
-              <select className="form-control">
+              <select
+                className="form-control"
+                onChange={(e) => setRole(e.target.value)}
+              >
                 <option value="USER">USER</option>
                 <option value="ADMIN">ADMIN</option>
-                <option selected>Choose...</option>
               </select>
             </div>
             <div className=" col-md-12">
-              <label>Image</label>
-              <input type="file"></input>
+              <label className="form-label lable-upload" htmlFor="lableUpload">
+                <FaArrowCircleUp />
+                Upload Image
+              </label>
+              <input
+                type="file"
+                hidden
+                id="lableUpload"
+                onChange={(e) => handelUploadImage(e)}
+              />
+            </div>
+            <div className=" col-md-12 img-preview">
+              {previewImage ? (
+                <img src={previewImage} />
+              ) : (
+                <span>Preview Image</span>
+              )}
             </div>
           </form>
         </Modal.Body>
@@ -58,7 +136,7 @@ function Example() {
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={() => handlSubmitCreateUser()}>
             Save Changes
           </Button>
         </Modal.Footer>
@@ -66,4 +144,4 @@ function Example() {
     </>
   );
 }
-export default Example;
+export default ModelCreateUser;
